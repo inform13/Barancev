f3c0fd2c91cc25d60d91e099d10bd338530726dc
KaterinaY <33397680+inform13@users.noreply.github.com>
2020-02-22 22:08:33 +0300

    Update SquareTests.java

--- sandbox/src/test/java/ru/stqua/baran/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqua/baran/sandbox/SquareTests.java
@@ -1,8 +1,7 @@
 package ru.stqua.baran.sandbox;
 
-
-import org.testng.annotations.Test;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 import ru.stqa.baran.sandbox.Square;
 
 public class SquareTests {
@@ -10,6 +9,7 @@ public class SquareTests {
   @Test
   public void testArea() {
     Square s = new Square(5);
+    System.out.println();
     Assert.assertEquals(s.area(), 25.0);
   }
 }
